a316a82a7af563144a71621839a73d9533ac95c8
Neyo Utha <117210225+neyo-utha@users.noreply.github.com>
2023-02-25 13:12:38 -0500

    Add files via upload

---
+++ Assignment 4: Trees and Binary Search Trees/Files/jsjf/exceptions/NonComparableElementException.java
@@ -0,0 +1,25 @@
+package jsjf.exceptions;
+
+/**
+ * @author Neyoshaan Uthayakumaran TRU : T00665922
+ */
+
+/**
+ * NonComparableElementException  represents the situation in which an attempt
+ * is made to add an element that is not comparable to an ordered collection
+ *
+ * @author Java Foundations
+ * @version 4.0
+ */
+public class NonComparableElementException extends RuntimeException
+{
+    /**
+     * Sets up this exception with an appropriate message.
+     *
+     * @param collection  the exception message to deliver
+     */
+    public NonComparableElementException (String collection)
+    {
+        super ("The " + collection + " requires Comparable elements.");
+    }
+}
